caa2f36a3d65331d23eab57384fda0d849a5f91a
Jim Knight <jim.knight@lavatech.com>
2012-04-27 19:44:47 -0400

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agility::Application.config.secret_token = '515749f840789e9f84dada24b48b6d35cad611389d982b0e666afb38d6c58707a134a72df265cd347eb2847177d43a7fa92080e73e7169dc9dee2ed9ab310b92'
