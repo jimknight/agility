57bf906d6271afcbea23fb42947e19a3c10ae4a8
Jim Knight <jim.knight@lavatech.com>
2012-04-27 21:04:45 -0400

    removed factories dir

--- spec/factories/attachments.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :attachment do
-    file "MyString"
-    attachable_id 1
-    attachable_type "MyString"
-  end
-end
