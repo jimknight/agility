eb92c401a2977fa4422a58a37625b8531d7c11d1
Jim Knight <jim.knight@lavatech.com>
2012-05-28 19:09:31 -0400

    messages: added index

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -1,18 +1,24 @@
 require 'spec_helper'
 
 describe "Messages" do
-  describe "GET /projects/1/messages" do
-    it "should show messages for a project" do
-    	@user = sign_in_as("user@example.com","abc123")
-    	@project = FactoryGirl.create(:project)
-    	5.times do
-    		@message = FactoryGirl.create(:message, :content => "chatterbug")
-    		@project.messages << @message
-        @user.messages << @message  		
-    	end
-      visit project_messages_path(@project)
-      page.should have_content("chatterbug")
-      page.should have_content(@user.email)
+  before(:each) do
+    @user = sign_in_as("user@example.com","abc123")
+    @project = FactoryGirl.create(:project)
+    5.times do |i|
+      @message = FactoryGirl.create(:message, :content => "chatterbug#{i}")
+      @project.messages << @message
+      @user.messages << @message      
     end
+  end
+  it "should show messages for a project" do
+    visit project_messages_path(@project)
+    page.should have_content("chatterbug")
+    page.should have_content(@user.email)
+  end
+  it "can be searched" do
+    pending
+    visit project_messages_path(@project)
+    fill_in "query", :with => "chatterbug1"
+    page.should have_link("chatterbug1", :href => project_messages_path(@project))
   end
 end
