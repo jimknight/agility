dc31d59f0b1cc08a4cdaa4f0b8c28d257b9fbf02
Jim Knight <jim.knight@lavatech.com>
2012-06-29 10:26:22 -0400

    projects: adding users progression but want to rethink this

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,25 +1,7 @@
 class ProjectsController < ApplicationController  
   
-  #load_and_authorize_resource # :through => :current_user
-
-  def user_search
-    @project = Project.find(params[:id])
-    @user = User.find_by_email(params[:email])
-    if @user
-      if @project.user_id == @user.id  || @project.users.map(&:id).include?(@user.id)
-        redirect_to @project, :notice => "#{@user.email} is already a team member of this project"
-      else
-        @project.users << @user
-        redirect_to @project, :notice => "#{@user.email} added to this project"
-      end
-    else
-      redirect_to @project, :notice => "#{params[:email]} couldn't be found. Please invite them to join." # TODO give a way to send a register request
-    end
-  end
-
   def show
-    # empty because work done by load_and_authorize_resource
-   @project = Project.find(params[:id]) # remove this when load/auth re-enabled
+    @project = Project.find(params[:id])
     @tasks = @project.tasks
     @notes = @project.notes
     @emails = @project.emails
@@ -32,35 +14,24 @@ class ProjectsController < ApplicationController
   end
   
   def index
-     # empty because work done by load_and_authorize_resource
-     # show the projects the user owns or is a member
-    #@projects = Project.all # just for testing for now
     @projects = Project.user_can_read(current_user)
   end
 
-  # GET /projects/new
-  # GET /projects/new.xml
   def new
-    @project = Project.new
-  
+    @project = Project.new  
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @project }
     end
   end
 
-  # GET /projects/1/edit
   def edit
     @project = Project.find(params[:id])
-   # authorize! :edit, @project
   end
 
-  # POST /projects
-  # POST /projects.xml
   def create
     @project = Project.new(params[:project])
     current_user.projects << @project
-
     respond_to do |format|
       if @project.save
         format.html { redirect_to(@project, :notice => 'Project was successfully created.') }
@@ -72,12 +43,8 @@ class ProjectsController < ApplicationController
     end
   end
 
-  # PUT /projects/1
-  # PUT /projects/1.xml
   def update
     @project = Project.find(params[:id])
-#    authorize! :update, @project
-
     respond_to do |format|
       if @project.update_attributes(params[:project])
         format.html { redirect_to(@project, :notice => 'Project was successfully updated.') }
@@ -89,5 +56,21 @@ class ProjectsController < ApplicationController
     end
   end
 
+  # NON-REST ACTIONS
+
+  def user_search
+    @project = Project.find(params[:id])
+    @user = User.find_by_email(params[:email])
+    if @user
+      if @project.user_id == @user.id  || @project.users.map(&:id).include?(@user.id)
+        redirect_to @project, :alert => "#{@user.email} is already a team member of this project"
+      else
+        @project.users << @user
+        redirect_to @project, :notice => "#{@user.email} added to this project"
+      end
+    else
+      redirect_to @project, :alert => "#{params[:email]} couldn't be found. Please invite them to join." # TODO give a way to send a register request
+    end
+  end
 
 end
