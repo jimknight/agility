454aa5d8c162946d670885e926c92252d5caa679
Jim Knight <jim.knight@lavatech.com>
2013-02-13 17:55:13 -0500

    changing agilechamp to new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,13 @@
 require "bundler/capistrano"
 load 'deploy/assets' # this is a rails 3.0 app
 
-set   :domain,        "69.172.229.224"
+set   :domain,        "69.164.212.223"
 role  :web,           domain
 role  :app,           domain
 role  :db,            domain, :primary => true
 
 # Fill user in - if remote user is different to your local user
-set :user, "rails"
+set :user, "deployer"
 
 default_run_options[:pty] = true
 
